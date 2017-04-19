b39ae4ab30957832d7f29894a3a9c0d5bbc09a62
Andrew Klotz <agc.klotz@gmail.com>
2017-04-18 23:13:19 -0400

    mix new stream_listener --module StreamListener

---
+++ stream_listener/lib/stream_listener.ex
@@ -0,0 +1,18 @@
+defmodule StreamListener do
+  @moduledoc """
+  Documentation for StreamListener.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> StreamListener.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ stream_listener/test/stream_listener_test.exs
@@ -0,0 +1,8 @@
+defmodule StreamListenerTest do
+  use ExUnit.Case
+  doctest StreamListener
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
