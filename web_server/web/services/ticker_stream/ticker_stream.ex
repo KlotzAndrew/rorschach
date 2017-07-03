f0439e41a94bb039b1c0f126465c388dafe6e0c3
Andrew Klotz <agc.klotz@gmail.com>
2017-07-03 18:44:35 -0400

    ticker stream async instead of sync

--- web_server/web/services/ticker_stream/ticker_stream.ex
+++ web_server/web/services/ticker_stream/ticker_stream.ex
@@ -7,12 +7,8 @@ defmodule WebServer.TickerStream do
   # @market "http://market:5000"
   # @demo_fields "&field=4+10+11"
 
-  def start_link(opts \\ []) do
-    GenServer.start_link(__MODULE__, :ok, opts)
-  end
-
-  def init(:ok) do
-    {:ok, listen()}
+  def start_link(_opts \\ []) do
+    GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
   end
 
   def listen do
@@ -22,5 +18,23 @@ defmodule WebServer.TickerStream do
 
   def parse_tick(tick) do
     TickHandler.parse(tick)
+  end
+
+  # Callbacks
+
+  def init(:ok) do
+    GenServer.cast(self(), :listen)
+    {:ok, []}
+  end
+
+  def handle_cast(:listen, state) do
+    kafka_stream = KafkaEx.stream("test", 0, offset: 0, auto_commit: false)
+    Enum.each(kafka_stream, fn(message) -> TickHandler.parse(message.value) end)
+
+    IO.puts "Listen block failed!"
+    :timer.sleep(5_000)
+    GenServer.cast(self(), :listen)
+
+    {:noreply, state}
   end
 end
