8a040ac0178fa27093aef3d7258f8b20a7e751fe
Andrew Klotz <agc.klotz@gmail.com>
2017-07-25 21:11:04 -0400

    track hackney response id for streaming

--- web_server/web/services/ticker_stream/market_consumer.ex
+++ web_server/web/services/ticker_stream/market_consumer.ex
@@ -1,7 +1,7 @@
 defmodule WebServer.MarketConsumer do
   use GenServer
 
-  alias WebServer.{MarketReceiver}
+  alias WebServer.{TickHandler}
 
   def start_link(_opts \\ []) do
     GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
@@ -10,12 +10,7 @@ defmodule WebServer.MarketConsumer do
   def init(:ok) do
     IO.puts "Starting stream..."
     :timer.send_after(2_000, :start_consume)
-    {:ok, []}
-  end
-
-  def listen do
-    kafka_stream = KafkaEx.stream("test", 0, offset: 0, auto_commit: false)
-    Enum.each(kafka_stream, fn(x) -> IO.inspect(x) end)
+    {:ok, nil}
   end
 
   # defp market_url, do: System.get_env("MARKET_URL")
@@ -26,13 +21,50 @@ defmodule WebServer.MarketConsumer do
   end
 
   def handle_info(:start_consume, state) do
-    {:ok, HTTPoison.get(url(), %{}, [timeout: :infinity, stream_to: MarketReceiver])}
-    IO.inspect "TODO: MarketConsumer crash handler"
-    {:stop, :timeout, state}
+    if state do
+      :hackney.stop_async state # TODO: can this error?
+    end
+
+    {:ok, %HTTPoison.AsyncResponse{id: id}} =
+      HTTPoison.get(url(), %{}, [recv_timeout: :infinity, stream_to: self()])
+
+    {:noreply, id}
+  end
+
+  def handle_info(%HTTPoison.AsyncResponse{id: id}, state) do
+    IO.inspect id, label: "HTTPoison.AsyncResponse: "
+    {:noreply, state}
+  end
+
+  def handle_info(%HTTPoison.AsyncStatus{code: code}, state) do
+    IO.inspect code, label: "AsyncStatus: "
+    {:noreply, state}
+  end
+
+  def handle_info(%HTTPoison.AsyncHeaders{headers: headers}, state) do
+    IO.inspect headers, label: "AsyncHeaders: "
+    {:noreply, state}
+  end
+
+  def handle_info(%HTTPoison.AsyncChunk{chunk: chunk}, state) do
+    TickHandler.parse(chunk)
+    {:noreply, state}
+  end
+
+  def handle_info(%HTTPoison.AsyncEnd{id: id}, _state) do
+    IO.inspect id, label: "HTTPoison.AsyncEnd: "
+    :timer.send_after(2_000, :start_consume)
+
+    {:noreply, nil}
+  end
+
+  def handle_info(%HTTPoison.Error{reason: reason}, state) do
+    IO.inspect reason, label: "HTTPoison.Error: "
+    {:noreply, state}
   end
 
   def handle_info(msg, state) do
-    IO.inspect msg, "MarketConsumer unknown message"
+    IO.inspect msg, label: "MarketConsumer unknown message"
     {:noreply, state}
   end
 end
